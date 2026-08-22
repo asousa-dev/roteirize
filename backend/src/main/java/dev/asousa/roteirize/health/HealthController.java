999595f27ad871ff1626a428ae6fa8bb17cf33da
Arthur Sousa <arthurzico1304@gmail.com>
2026-08-21 23:21:48 -0300

    feat: adiciona endpoint de saúde da API

---
+++ backend/src/main/java/dev/asousa/roteirize/health/HealthController.java
@@ -0,0 +1,12 @@
+package dev.asousa.roteirize.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController{
+    @GetMapping("/api/v1/health")
+    public String health(){
+        return "API do Roteirize conectada";
+    }
+}
